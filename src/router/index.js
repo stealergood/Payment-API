1889f474b12b0d0990a3bf63c9ef7aa4ae4f1d8c
Denny Edy Saputra <ncstealer@gmail.com>
2023-12-28 22:42:37 +0700

    Changes to be committed: 	modified:   .env 	modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,4 +4,4 @@ import { Payment, TRXStatus } from "../controller/payment.js";
 export const router = express.Router();
 
 router.post("/api/v1/payment", Payment);
-router.get("/api/v1/payment/status", TRXStatus);
+router.post("/api/v1/payment/status", TRXStatus);
